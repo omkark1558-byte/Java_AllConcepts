6eeb243bc92c61dd4129ef0c3120736de59c0241
omkark1558-byte <omkark1558>
2025-11-05 15:01:42 +0530

    String

---
+++ String/String_02.java
@@ -0,0 +1,67 @@
+package String;
+
+public class String_02 {
+
+	public static void main(String[] args) {
+		 
+		String You=new String ("OMKAR");
+	
+		String Name="Moon_Difine";
+		
+//		System.out.println(You.toUpperCase());
+//		System.out.println(You.toLowerCase());
+		
+//		System.out.println(You.lines());
+//		System.out.println(You.length());
+//		System.out.println(Name.getClass());
+	
+//		char charc=You.charAt(4);
+		
+//		System.out.println(charc);
+		
+//		System.out.println(Name);
+		
+		String sen=new String("Hlw What is Your Names & YOu Add...@");
+		
+		String sen1=sen.toUpperCase();
+		
+//		System.out.println(sen1.toLowerCase());
+//		sen1.valueOf(12);
+//		System.out.println(sen1);
+		
+		int o=10;
+		int o1=2;
+		
+//		System.out.println(o==o1);
+		
+		String Nameee="Omaa";
+		
+		String Uo="Jikk";
+		
+		
+//		System.out.println(Nameee.equals(Uo));
+		
+		char[] you1=You.toCharArray();
+		for(int i=0;i<you1.length;i++)
+		{
+			System.out.print(you1[i]+" ");	
+		}
+		
+		System.out.println();
+		System.out.println(You.equals(Name));
+		
+		
+		
+//		System.out.println(You.equalsIgnoreCase(Name));
+		
+		
+		
+	}
+}
+/* . Literal
+ * . length()
+ *  cahrAt(int)=> char
+ * .equal();
+ * 
+ */
+
---
+++ String/string_01.java
@@ -0,0 +1,30 @@
+package String;
+
+import java.util.Scanner;
+
+
+public class string_01 {
+
+	public static void main(String[] args) {
+		 
+		Scanner sc=new Scanner(System.in); 
+		
+//		double Marks2=20.00;
+		System.out.println("Enter a Marks...!");	
+		double Marks =sc.nextDouble();
+		
+		if(Marks>35 && Marks<=45)
+		{
+			System.out.println("Student Is Failled..!");
+		}else if(Marks >45 && Marks <=65)
+		{
+			System.out.println("Student is Passed..");
+		}else if(Marks >65 && Marks <=85)
+		{
+			System.out.println("Student is Pass And Student Marks is very Good >> Congratulations");
+		}else
+		{
+			System.out.println("<<<...Invaild marks...* Enter a vaild marks..>>>");
+		}
+	}
+}
