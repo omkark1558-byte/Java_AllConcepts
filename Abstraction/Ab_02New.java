c2a2120c5ea3c5dc48447292eba1169d2318bf98
omkark1558-byte <omkark1558>
2025-11-11 22:01:49 +0530

    Abstraction Program :

--- Abstraction/Ab_02New.java
+++ Abstraction/Ab_02New.java
@@ -3,21 +3,67 @@ package Abstraction;
 public class Ab_02New {
 
 	public static void main(String[] args) {
-		 
-		Ioj y=new You();
-		y.To();
+
+		Jio h=new Jio1();
+		h.SIM(0);
+		h.Network("Numbers");
+		h.Owner("Ok");
+		
+		System.out.println("***            ***");
+		
+		Jio p=new Jio2();
+		p.Network("Ho");
+		p.Owner("Hok");
+		p.SIM(3);
+		
+		
+	}
+}
+abstract class Jio
+{
+	public String Name="Owner";
+	
+	public abstract void SIM(int i);
+	
+	abstract void Network(String st);
+	
+	abstract void Owner(String yo);
+}
+
+class Jio1 extends Jio
+{
+	public void SIM(int i)
+	{
+		System.out.println("SIM Is Jio Company..!");
+	}
+	public void Network(String st)
+	{
+		System.out.println("< SIM & Company Net Worth is All India..@ >");
+	}
+	public void Owner(String yo)
+	{
+		System.out.println(": Is Compane Owner Of Ananta Ambani..! >");
 	}
 }
 
-interface Ioj
+class Jio2 extends Jio
 {
-	void To();
-	
-}
-class You implements Ioj
-{
-	public void To()
-	{
-		System.out.println("Hlwww...!");
+
+	@Override
+	public void SIM(int i) {
+		 
+		System.out.println("Nettwork .....!");
 	}
+
+	@Override
+	void Network(String st) {
+		 
+		System.out.println("Jioooo.....!");
+	}
+
+	@Override
+	void Owner(String yo) {
+		 
+		System.out.println("<.AMBANI.>");
+	}	
 }
