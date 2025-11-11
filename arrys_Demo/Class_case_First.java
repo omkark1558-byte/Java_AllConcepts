628cfc95880907c7bd0a731daf0f5ce56f1a7e56
omkark1558-byte <omkark1558>
2025-11-11 12:45:25 +0530

    arry con

--- arrys_Demo/Class_case_First.java
+++ arrys_Demo/Class_case_First.java
@@ -4,24 +4,28 @@ public class Class_case_First {
 
 	public static void main(String[] args) {
 		 
-		Travels Seat=new Travels("Seat is ",02);
-		
+		Travels S1=new Travels("Seat is ",02);
+		S1.s2();
 		
 	}
-
 }
 
 class Travels
 {
 	int Seat;
-	public Travels(String Seat,int Number)
+	public Travels(String S,int N)
 	{
 		System.out.println("All Seat's is Booked !...");
 	}
 	
-	public void seats() 
+	public void seat() 
 	{
 		System.out.println(Seat);
 	}
-	
+	public void s2()
+	{
+		System.out.println("s2 is Travel Class.");
+	}
 }
+
+
