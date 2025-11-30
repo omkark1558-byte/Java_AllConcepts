4ac0c90ff0edc8580c29e8b41b668c0d899b2f9a
omkark1558-byte <omkark1558@gmail.com>
2025-11-30 11:14:43 +0530

    status Code..1

--- Status_Code/Status_01.java
+++ Status_Code/Status_01.java
@@ -5,10 +5,19 @@ public class Status_01 {
 	public static void main(String[] args) throws Exception
 	{
 		
-		Healthisnotok o = new Healthisnotok("Omkar");
-		
-		Health.Health_1(20);
-
+	try {
+		Health.Health_1(75);
+	}
+	catch(Healthcare F)
+	{
+		System.out.println("HealthCare Exception : "+F.getMessage());
+	}
+		catch(Healthisnotok S)
+	{
+	//	System.out.println("Healthisnotok Exception :" +S.getMessage());
+			S.printStackTrace();
+	}
+	 
 	}
 
 }
@@ -32,10 +41,10 @@ class Health
 	public static void Health_1(int Number) throws Healthcare,Healthisnotok
 	{
 		
-		if(Number >20 && Number < 60)
+		if(Number > 20 && Number < 60)
 		{
 			throw new Healthcare("Number Is Range Between In 20 < > 60");
-		}else if(Number >60 && Number < 85)
+		}else if(Number > 60 && Number < 85)
 		{
 			throw new  Healthisnotok("Number Range is the 60 TO 85");
 		}
