d3fce7e698cd5d52e67c88b6b0b01210da3d3571
omkark1558-byte <omkark1558@gmail.com>
2025-12-01 14:48:51 +0530

    Geric

---
+++ Genric/Genric_01.java
@@ -0,0 +1,42 @@
+package Genric;
+
+public class Genric_01 {
+
+	public static void main(String[] args) {
+	
+		ObjectAdder<String> uo = new ObjectAdder<String>();
+		
+		uo.add("100","200");
+		
+		System.out.println(uo.getinstance("TInkljaksjk"));
+	}
+}
+
+interface Iki<Y>
+{
+	void add(Y i1, Y i2);
+}
+
+
+class ObjectAdder<Y> implements Iki<Y>{
+	
+	public void add(Y i1, Y i2) {
+		if(i1 instanceof Integer && i2 instanceof Integer)
+		{
+			int u = (int) i1;
+			int o = (int) i2;
+			System.out.println(u+o);
+		}else if(i1 instanceof String && i2 instanceof String){
+			String u = (String)i1;
+			String o = (String)i2;
+			System.out.println(u+o);
+		}
+	}
+	public int getinstance(Y o){
+		if(o instanceof String)
+		{
+			return((String)o).length();
+		}
+		return 0;
+	}
+}	
