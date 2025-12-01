47ac87d9b5a08e966db942b97bd2abd8c18eeaaf
omkark1558-byte <omkark1558@gmail.com>
2025-12-01 23:33:55 +0530

    Genric Cons 04

--- Genric/Genric_04.java
+++ Genric/Genric_04.java
@@ -3,30 +3,48 @@ package Genric;
 public class Genric_04 {
 
 	public static void main(String[] args) {
- 
 
-		Wanted I = new Wanted1();
-		I.aroapi("302", "Nandakishor");
+		Wanted<String, Integer> I = new Wanted1();
+		System.out.println("Wanted01 Output: " + I.aroapi("22", "OMKAR"));
+
+		Wanted<String[], String[]> w2 = new Third();
+
+		String[] arr1 = { "OMAKR", "VISHAL" };
+		String[] arr2 = { "GENRICS", "PROGRAM" };
+
+		String[] result = w2.aroapi(arr1, arr2);
+
+		System.out.print("Third Out-Put");
+		for (String f : result) {
+			System.out.print(f + " ");
+		}
+
 	}
 }
 
-interface Wanted
-{
-	void aroapi(String N,String Age);
+interface Wanted<Y, H> {
+	H aroapi(Y N1, Y Age1);
 }
 
-class Wanted1 implements Wanted
-{
-	public void aroapi(String N,String Age)
-	{
-		System.out.println("Age OF Wanted...! " + N+Age);
+class Wanted1 implements Wanted<String, Integer> {
+	public Integer aroapi(String N1, String Age1) {
+		return N1.length() + Age1.length();
 	}
 }
 
-class Third extends Wanted1
-{
-	public void aroapi(String N,String Age)
-	{
-		System.out.println(N+Age);
+class Third implements Wanted<String[], String[]> {
+	public String[] aroapi(String[] N1, String[] Age1) {
+		String[] result = new String[N1.length + Age1.length];
+
+		int i = 0;
+
+		for (String f : N1)
+			result[i++] = f;
+
+		for (String f : Age1)
+			result[i++] = f;
+
+		return result;
+
 	}
 }
