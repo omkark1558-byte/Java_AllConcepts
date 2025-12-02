878fe0bdb0d06f21f0fde665659cdeada250d1dc
omkark1558-byte <omkark1558@gmail.com>
2025-12-02 19:44:40 +0530

    Colllection..!

---
+++ Colllection/Collection_01.java
@@ -0,0 +1,30 @@
+package Colllection;
+
+import java.util.ArrayList;
+
+public class Collection_01 {
+
+	public static void main(String[] args) {
+		 
+
+		 ArrayList<Integer> arr = new ArrayList<Integer>();
+		  
+		arr.add(20);
+		arr.add(25);
+		arr.add(32);
+		arr.add(45);
+		arr.add(560);
+		arr.add(null);
+		arr.add(30);
+	
+		
+		System.out.println(arr);
+
+		System.out.println(arr.get(2));
+		
+		System.out.println(arr.getFirst());
+		
+		System.out.println(arr.getLast());
+		
+	}
+}
