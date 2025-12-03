6cb767be82eeb8da882379367d5fe87a711cee0c
omkark1558-byte <omkark1558@gmail.com>
2025-12-03 23:46:36 +0530

    : Operaters...:

--- Operators/First.java
+++ Operators/First.java
@@ -5,5 +5,16 @@ public class First {
 	public static void main(String[] args) {
 		
 		System.out.println("Hiii......!");
+		
+		
+		int[] Trr = {20,45,65,75,49};
+		
+		int sum = 0;
+		
+		for(int v : Trr)
+		{
+			sum += v;
+		}
+		System.out.println("Sum of Numbers : "+ sum);
 	}
 }
