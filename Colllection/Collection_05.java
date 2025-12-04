9038dab5ccfead887786ff963242e1d9a5a574b3
omkark1558-byte <omkark1558@gmail.com>
2025-12-04 10:09:34 +0530

    Collection

---
+++ Colllection/Collection_05.java
@@ -0,0 +1,23 @@
+package Colllection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Collection_05 {
+
+	public static void main (String args[])
+	{
+		List<Integer> list = new ArrayList<Integer>();
+		 
+		list.add(10);
+		list.add(20);
+		list.add(25);
+		list.add(25);
+		list.add(10);
+		
+		System.out.println("Modified List : " + list);
+		
+	}
+}
+
+// class 
