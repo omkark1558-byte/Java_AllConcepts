478f3265d6e6dd8cd220d47b0f9dbc4cca9963cd
omkark1558-byte <omkark1558@gmail.com>
2025-12-07 22:27:36 +0530

    Collection..[]

---
+++ Colllection/Collection_06.java
@@ -0,0 +1,33 @@
+package Colllection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Collection_06 {
+
+	public static void main(String[] args) {
+		 
+		
+		List<Bank> U = new ArrayList<Bank>();
+		
+		Bank t = new Bank();
+		t.Number=1;
+		t.Name="Akshay More";
+		t.Price = 200.5;
+		
+		U.add(t);
+		
+		for (Bank b : U)
+		{
+			System.out.println(b.Name +" "+ b.Number + " "+b.Price);
+		}
+	}
+}
+
+class Bank
+{
+	String Name;
+	double Price;
+	int Number;
+}
