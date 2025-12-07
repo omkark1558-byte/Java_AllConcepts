3d7b87f746564a4778e6c06278a6aaabb333d3f3
omkark1558-byte <omkark1558@gmail.com>
2025-12-07 22:34:23 +0530

    Collection

---
+++ Colllection/Collection_08.java
@@ -0,0 +1,24 @@
+package Colllection;
+
+import java.util.*;
+
+public class Collection_08 {
+
+	public static void main(String[] args) {
+
+		List<Integer> list = new ArrayList<>();
+
+		list.add(10);
+		list.add(12);
+		list.add(15);
+		list.add(17);
+		list.add(19);
+		list.add(20);
+
+		System.out.println(list.get(1));
+
+		System.out.println("Size = : " + list.size());
+
+	}
+
+}
