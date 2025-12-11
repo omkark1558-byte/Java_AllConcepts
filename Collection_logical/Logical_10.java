794ce8db13ea2c564fbc71544749a2865f218f78
omkark1558-byte <omkark1558@gmail.com>
2025-12-12 00:26:52 +0530

    Colleoction []

---
+++ Collection_logical/Logical_10.java
@@ -0,0 +1,33 @@
+package Collection_logical;
+
+import java.util.*;
+
+public class Logical_10 {
+
+	public static void main(String[] args) {
+		 
+
+		
+		ArrayList<Integer> List = new ArrayList<>(2);
+
+		List.add(10); List.add(10); List.add(20); List.add(30);
+		List.add(10);
+		List.add(55); List.add(44);
+		
+		System.out.println("First Array Index In Index :"+ List.get(0));
+		System.out.println("Last Index Of Array List"+List.getLast());
+		System.out.println("Array list .. :- "+List);
+		
+		/*
+		 * .getFirst Show the array List of First Index..!
+		 * .get Last show the Array List of Last Index..!
+		 */
+	
+		
+	 
+//		System.out.println(List.remove(1));
+		
+	}
+
+}
+ 
