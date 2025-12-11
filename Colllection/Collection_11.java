65ad587502686d441958fb94ec322a9054172e49
omkark1558-byte <omkark1558@gmail.com>
2025-12-12 00:30:48 +0530

    Collections :

---
+++ Colllection/Collection_11.java
@@ -0,0 +1,24 @@
+package Colllection;
+import java.util.*;
+
+public class Collection_11 {
+
+	public static void main(String[] args) {
+	
+		
+		Vector<Integer> list = new Vector<Integer>();
+
+		Collections.addAll(list, 20,30,40,50);
+		
+		ArrayList<Object> list1 = new ArrayList<Object>();
+		
+		Collections.addAll(list1, 1,2,3,5);
+		
+		System.out.println(list);
+		
+//		System.out.print("");
+		
+		System.out.print(list1);
+		 
+	}
+}
