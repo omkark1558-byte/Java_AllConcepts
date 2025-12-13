a3a4c292d401a741a09f8dc2c9d2d49947cdc957
omkark1558-byte <omkark1558@gmail.com>
2025-12-13 22:29:24 +0530

    Logical -[]

---
+++ Collection_logical/Logical_16.java
@@ -0,0 +1,11 @@
+package Collection_logical;
+
+public class Logical_16 {
+
+	public static void main(String[] args) {
+
+		Stude s1 = new Stude(1, "OMKAR", 500.20);
+		System.out.println(s1.toString());
+
+	}
+}
