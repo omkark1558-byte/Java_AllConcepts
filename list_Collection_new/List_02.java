6b3bfba1dbac69707b52d5fc82a33125f1e11a1c
omkark1558-byte <omkark1558@gmail.com>
2025-12-15 22:50:44 +0530

    List program

--- list_Collection_new/List_02.java
+++ list_Collection_new/List_02.java
@@ -1,4 +1,4 @@
-package Colllection;
+package list_Collection_new;
 
 import java.util.*;
 
@@ -14,48 +14,40 @@ public class List_02 {
 		list.add(4);
 		list.add(5);
 
-		
 		System.out.println("------- Integer list -------");
-		for(Integer A : list)
-		{
+		for (Integer A : list) {
 			System.out.println(A);
 		}
-		
+
 		ArrayList<String> list2 = new ArrayList<>();
-		
-		Collections.addAll(list2, "O","M","I","I","P","A","U","I","O");
-		
-		System.out.println("--------- Word list --------"
-				+ "");
-		
-		for(String m : list2)
-		{
+
+		Collections.addAll(list2, "O", "M", "I", "I", "P", "A", "U", "I", "O");
+
+		System.out.println("--------- Word list --------" + "");
+
+		for (String m : list2) {
 			System.out.println(m);
 		}
-		
+
 		ArrayList<Bank1> liest = new ArrayList<>();
-		
-		
-		 Collections.addAll(liest, new Bank1( 10,"OMAKR",2000.00));
-				 System.out.println(liest);
+
+		Collections.addAll(liest, new Bank1(10, "OMAKR", 2000.00));
+		System.out.println(liest);
 	}
 }
 
-class Bank1
-{
+class Bank1 {
 	int id;
 	String name;
 	double balance;
-	
-	Bank1(int id,String name,double balance)
-	{
+
+	Bank1(int id, String name, double balance) {
 		this.balance = balance;
 		this.id = id;
 		this.name = name;
 	}
-	
-	public String toString()
-	{
-		return this.id+" "+this.name+" "+this.balance;
+
+	public String toString() {
+		return this.id + " " + this.name + " " + this.balance;
 	}
 }
