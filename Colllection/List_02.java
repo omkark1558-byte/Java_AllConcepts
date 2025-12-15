abd69e9f50f78b2b9a83709d505482c27044b026
omkark1558-byte <omkark1558@gmail.com>
2025-12-15 21:42:31 +0530

    Logiacl 2 Array Object

--- Colllection/List_02.java
+++ Colllection/List_02.java
@@ -5,16 +5,57 @@ import java.util.*;
 public class List_02 {
 
 	public static void main(String[] args) {
-	
+
 		ArrayList<Integer> list = new ArrayList<>();
+
+		Collections.addAll(list, 0, 1, 2);
+
+		list.add(3);
+		list.add(4);
+		list.add(5);
+
 		
-	//	Collections.addAll(10);
-		list.add(10);
-		list.add(null);
-		list.add(null);
+		System.out.println("------- Integer list -------");
+		for(Integer A : list)
+		{
+			System.out.println(A);
+		}
+		
+		ArrayList<String> list2 = new ArrayList<>();
+		
+		Collections.addAll(list2, "O","M","I","I","P","A","U","I","O");
+		
+		System.out.println("--------- Word list --------"
+				+ "");
+		
+		for(String m : list2)
+		{
+			System.out.println(m);
+		}
+		
+		ArrayList<Bank1> liest = new ArrayList<>();
 		
 		
-		System.out.println(list);
+		 Collections.addAll(liest, new Bank1( 10,"OMAKR",2000.00));
+				 System.out.println(liest);
 	}
 }
 
+class Bank1
+{
+	int id;
+	String name;
+	double balance;
+	
+	Bank1(int id,String name,double balance)
+	{
+		this.balance = balance;
+		this.id = id;
+		this.name = name;
+	}
+	
+	public String toString()
+	{
+		return this.id+" "+this.name+" "+this.balance;
+	}
+}
