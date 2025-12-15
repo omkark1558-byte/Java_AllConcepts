8d1ab7821af04ebbad8e42063274628e5e15017b
omkark1558-byte <omkark1558@gmail.com>
2025-12-15 14:07:56 +0530

    : Logical [] :

---
+++ Collection_logical/Logical_18.java
@@ -0,0 +1,17 @@
+package Collection_logical;
+import java.util.*;
+
+public class Logical_18 {
+
+	public static void main(String[] args) {
+		 
+		HashMap<Integer, String> map = new HashMap<>();
+		
+		map.put(101, "OMKAR KALE");
+		map.put(102,"NAN");
+		
+		System.out.println(map);
+	}
+
+}
+ 
