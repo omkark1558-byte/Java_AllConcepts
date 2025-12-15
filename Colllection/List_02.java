666ab56853ff82b9e9e8587f4069bb945b541e2b
omkark1558-byte <omkark1558@gmail.com>
2025-12-15 17:34:57 +0530

    Collection Logical

---
+++ Colllection/List_02.java
@@ -0,0 +1,20 @@
+package Colllection;
+
+import java.util.*;
+
+public class List_02 {
+
+	public static void main(String[] args) {
+	
+		ArrayList<Integer> list = new ArrayList<>();
+		
+	//	Collections.addAll(10);
+		list.add(10);
+		list.add(null);
+		list.add(null);
+		
+		
+		System.out.println(list);
+	}
+}
+
