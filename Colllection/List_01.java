2cc96989fb1e9f3730846ccebff2f8def4feb0f8
omkark1558-byte <omkark1558@gmail.com>
2025-12-15 17:33:29 +0530

    List logical

---
+++ Colllection/List_01.java
@@ -0,0 +1,38 @@
+package Colllection;
+
+import java.util.*;
+
+public class List_01 {
+
+	public static void main(String[] args) {
+		 
+		ArrayList<String> i = new ArrayList<String>();
+		i.add("10");
+		i.add("O");
+		i.add("M");
+		i.add("K");
+		i.add("5");
+		i.add("A");
+//		i.addAll(Arrays.asList("23","54"));
+		
+		System.out.println(i);
+	}
+}
+class Employee
+{
+	int id;
+	String name;
+	double salary;
+	
+	Employee(int id, String name, double salary)
+	{
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+	
+	public String toString()
+	{
+		return this.id + this.name + this.salary;
+	}
+}
