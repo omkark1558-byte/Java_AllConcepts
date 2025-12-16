c4687b0d8bd475d1724bc8edd7e60505bbb1eed9
omkark1558-byte <omkark1558@gmail.com>
2025-12-16 22:10:42 +0530

    Logical

--- Collection_logical/Logical_19.java
+++ Collection_logical/Logical_19.java
@@ -6,19 +6,28 @@ public class Logical_19 {
 
 	public static void main(String[] args) {
 
-		
 // ----------- Remove Duplicates Elements in List Using The Set Method ------------
-		
+
 		List<Integer> list = new ArrayList<Integer>();
-		
-		Collections.addAll(list, 01,02,03,04,05,06,05,04,02,01,03);
-		
+
+		Collections.addAll(list, 01, 02, 03, 04, 05, 06, 05, 04, 02, 01, 03);
+
 		Set<Integer> list1 = new HashSet<Integer>();
-		
+
+		Collections.addAll(list1, null, 01, null, 22, 34, 33, 23, 32, 32, 23, 56, 68);
+
 		list1.addAll(list);
-		
-		System.out.println("Orginal Array List: "+list);
-		System.out.println("Remove Elements List: "+list1);
+
+		Vector<String> i = new Vector<>();
+		Collections.addAll(i, "22", "76", "89", "100");
+
+		for(String h : i)
+		{
+			System.out.println(h);
+		}
+		System.out.println("Vector List [] : "+i);
+		System.out.println("Orginal Array List: " + list);
+		System.out.println("Remove Elements List: " + list1);
+
 	}
 }
- 
