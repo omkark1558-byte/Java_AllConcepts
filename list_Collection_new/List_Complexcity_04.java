b4eb0bc33a662cc56c9534efcc3f8732c3571bc3
omkark1558-byte <omkark1558@gmail.com>
2025-12-16 18:33:42 +0530

    Complexcitya

---
+++ list_Collection_new/List_Complexcity_04.java
@@ -0,0 +1,15 @@
+package list_Collection_new;
+
+public class List_Complexcity_04 {
+	
+	/*
+	 * Array List Time-Complexcite..! 
+	 */
+	
+	public static void main(String[] args) {
+
+		int[] arr  = {01,02,03,04,5,6,7,8,9,10};
+		 
+		System.out.println(arr[3]);
+	}
+}
