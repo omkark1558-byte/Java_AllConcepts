55715dd7d55e452f03a8ee2d61ab0c91ea8e91be
omkark1558-byte <omkark1558@gmail.com>
2025-12-16 18:38:02 +0530

    hascode

---
+++ list_Collection_new/List_complexcity_05.java
@@ -0,0 +1,23 @@
+package list_Collection_new;
+
+public class List_complexcity_05 {
+
+	public static void main(String[] args) {
+
+		int arr[] = { 1, 2, 3, 4, 5, 6 };
+
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(i);
+		}
+		
+		System.out.println("--------------------");
+		
+		String trr[] = { "A", "B", "C", "D", "E" };
+
+		for (String f : trr) {
+			System.out.println(f);
+		}
+
+	}
+
+}
