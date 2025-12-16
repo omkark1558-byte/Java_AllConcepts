b4314fcfbd9b7f0d6d522151266bbf93035ff06a
omkark1558-byte <omkark1558@gmail.com>
2025-12-16 23:11:06 +0530

    Logical

---
+++ list_Collection_new/Logicall_09.java
@@ -0,0 +1,22 @@
+package list_Collection_new;
+
+import java.util.LinkedHashSet;
+
+public class Logicall_09 {
+
+	public static void main(String[] args) {
+
+		LinkedHashSet<Integer> list = new LinkedHashSet<>();
+
+		System.err.println(list.add(10));
+
+		System.out.println(list.add(10));
+
+	}
+
+}
+
+/* Set Is Not Allowed The duplicate Eelements so 
+ * Because Answer is Boolean Value True / Flase :
+ */
+
